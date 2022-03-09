9d524775abb0e4358ee8087ee2e6f37922d8dbbe
RKhazaei <94053868+RKhazaei@users.noreply.github.com>
2022-03-09 11:33:40 +0100

    add

---
+++ Eventcountdown/main/main.js
@@ -0,0 +1,9 @@
+console.log("mainloaded")
+const timeInput = document.querySelector(".time-input")
+const button = document.querySelector(".submit")
+const countdownDiv = 
+const daySpan
+const hourSpan
+const minSpan
+const secSpan
+const announcementDiv

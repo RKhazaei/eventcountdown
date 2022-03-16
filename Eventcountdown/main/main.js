160e846e1161077caac9151dc1bbd0bc039b1b70
RKhazaei <302769656@student.rocmondriaan.nl>
2022-03-16 13:56:23 +0100

    small

--- Eventcountdown/main/main.js
+++ Eventcountdown/main/main.js
@@ -1,9 +1,9 @@
 console.log("mainloaded")
 const timeInput = document.querySelector(".time-input")
 const button = document.querySelector(".submit")
-const countdownDiv = 
-const daySpan
-const hourSpan
-const minSpan
-const secSpan
-const announcementDiv
+const countdownDiv = document.querySelector
+const daySpan = document
+const hourSpan = document
+const minSpan = document
+const secSpan = document
+const announcementDiv = document
